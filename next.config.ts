f5288dcd5cb633e5d0f181432ed933810bc79fe7
Korede <korede@gradpath.app>
2026-06-06 23:17:13 +0100

    fix: next config external packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "@anthropic-ai/sdk"],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
